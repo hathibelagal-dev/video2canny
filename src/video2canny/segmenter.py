1eb899e7465163ca41e38af213bf102816417c6e
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2025-08-21 23:32:35 +0530

    more features

--- src/video2canny/segmenter.py
+++ src/video2canny/segmenter.py
@@ -1,24 +1,41 @@
 from transformers import pipeline
 import torch
-import sys
 import numpy as np
 import os
 from PIL import Image
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-generator = pipeline(
-    "mask-generation", model="facebook/sam2-hiera-large", device=device)
+def generate_mask(image_path, invert = False):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    generator = pipeline(
+        "mask-generation",
+        model="facebook/sam2-hiera-large",
+        device=device
+    )
+    outputs = generator(image_path,  points_per_batch=16)
+    masks = outputs["masks"]
 
-image_path = sys.argv[1]
-outputs = generator(image_path,  points_per_batch=16)
-masks = outputs["masks"]
+    if len(masks) > 0:
+        mask = masks[0]
+        if invert:
+            mask = ~mask
+        img_array = np.array(
+            mask.cpu().numpy(), dtype=np.uint8) * 255
+        output_path = "mask_" + os.path.basename(image_path)
+        Image.fromarray(img_array, mode='L').save(output_path)
+        print(f"Mask saved to {output_path}")
+    else:
+        print("No masks found")
 
-if len(masks) > 0:
-    main_mask = masks[0]
-    img_array = np.array(outputs["masks"][0], dtype=np.uint8) * 255
-    output_path = "mask_" + os.path.basename(image_path)
-    Image.fromarray(img_array, mode='L').save(output_path)
-    print(f"Mask saved to {output_path}")
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("Usage: python mask_generator.py <image_path> [invert]")
+        sys.exit(1)
+    else:
+        image_path = sys.argv[1]
+        invert = False
+        if len(sys.argv) == 3:
+            if sys.argv[2] == "invert":
+                invert = True
+        generate_mask(image_path, invert)
 
-else:
-    print("No masks found")
